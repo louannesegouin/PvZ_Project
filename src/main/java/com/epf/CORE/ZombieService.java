4892bb65b34b16e734f3ac0e83a44492e78d6897
lsegouin <lou-anne.segouin@epfedu.fr>
2025-03-17 16:15:56 +0100

    V1

--- src/main/java/com/epf/CORE/ZombieService.java
+++ src/main/java/com/epf/CORE/ZombieService.java
@@ -1,13 +1,12 @@
 package com.epf.CORE;
 
-import com.epf.CORE.Zombie;
 import java.util.List;
 import java.util.Optional;
 
 public interface ZombieService {
-    Zombie create(Zombie zombie);
-    Optional<Zombie> findById(Long id);
-    List<Zombie> findAll();
-    Zombie update(Zombie zombie);
-    boolean deleteById(Long id);
+    Zombie create(Zombie zombie);                         // Créer un nouveau zombie
+    Optional<Zombie> findById(Long id);                   // Trouver un zombie par son ID
+    List<Zombie> findAll();                               // Récupérer tous les zombies
+    Zombie update(Zombie zombie);                         // Mettre à jour un zombie
+    boolean deleteById(Long id);                          // Supprimer un zombie par son ID
 }
